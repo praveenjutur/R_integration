1c40f339818e34da9e8818ce9e8548623517e6e0
Praveen <praveen4dream@gmail.com>
2017-11-20 14:49:34 -0500

    commit 03

--- Test_integration.R
+++ Test_integration.R
@@ -1,7 +1,20 @@
 #This is just a test script to understand th commit and push operations to GIT repository
-a<-1
-b<-2
-c<-a+b
-d<-c*5
-e<-c+d
-d<-e*b
+
+#' Title
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+f<function(x){
+ 
+  a<-1
+  b<-2
+  c<-a+b
+  d<-c*5
+  e<-c+d
+  d<-e*b
+  x<-d*2
+}
